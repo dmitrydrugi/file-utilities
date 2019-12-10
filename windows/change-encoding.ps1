0c3db6cc9c158fe075001783f7b4571c44f30895
Shaparev <dshaparev@KLTA.NET>
2019-12-10 19:28:12 +0100

    Refined the script to change file encoding

--- windows/change-encoding.ps1
+++ windows/change-encoding.ps1
@@ -1,11 +1,34 @@
-Write-Host "Hello, world"
-$Win1251Encoding = [System.Text.Encoding]::GetEncoding("windows-1251")
-$Utf8Encoding = [System.Text.Encoding]::GetEncoding("utf-8")
-# Get a list of the source files
-$FileList = Get-ChildItem -Path C:\temp -Recurse -File -Filter *.txt
-ForEach($File in $FileList) {
-	$Bytes = Get-Content -Path $File.FullName -Encoding Byte -ReadCount 0
-	$Content = [System.Text.Encoding]::Convert($Win1251Encoding, $Utf8Encoding, $Bytes)
-	Set-Content -Path $File.FullName $Utf8Encoding.getString($Content) -Encoding UTF8
+Write-Host "Hi there!"
+$SourceDirectory = "C:\temp"
+$FileFilter = "*.txt"
+$SourceEncodingName = "windows-1251"
+$DestinationEncodingName = "utf-8"
+$DestinationEncodingCode = "UTF8"
+try {
+	# Get encodings
+	$SourceEncoding = [System.Text.Encoding]::GetEncoding($SourceEncodingName)
+	$DestinationEncoding = [System.Text.Encoding]::GetEncoding($DestinatioEncodingName)
+	# Get a list of the source files
+	$FileCount = Get-ChildItem -Path $SourceDirectory -Recurse -File -Filter $FileFilter | Measure-Object | %{$_.Count}
+	$FileList = Get-ChildItem -Path $SourceDirectory -Recurse -File -Filter $FileFilter
+	$PercentComplete = 0
+	$FileIndex = 0
+	ForEach($File in $FileList) {
+		$FileIndex += 1
+		$PercentComplete = [math]::Round(($FileIndex / $FileCount) * 100)
+		Write-Progress -Activity "Changing of files encoding in progress" -Status "$PercentComplete% Complete:" -PercentComplete $PercentComplete
+		try {
+			$Bytes = Get-Content -Path $File.FullName -Encoding Byte -ReadCount 0
+			$Content = [System.Text.Encoding]::Convert($SourceEncoding, $DestinationEncoding, $Bytes)
+			Set-Content -Path $File.FullName $DestinationEncoding.getString($Content) -Encoding $DestinationEncodingCode
+		} catch {
+			Write-Error -Message ("Error occured during processing of " + $File.FullName + " file, Error: " + $_.Exception.Message) 
+				-Category ParserError -ErrorId "E-003"
+		}
+	}
+} catch [System.IO.IOException] {
+	Write-Error -Message ("Directory " + $SrcDir + " does not exist") -Category InvalidArgument -ErrorId "E-002"
+} catch {
+	Write-Error -Message $_.Exception.Message -Category InvalidArgument -ErrorId "E-001"
 }
 Read-Host "Press Enter to continue"
