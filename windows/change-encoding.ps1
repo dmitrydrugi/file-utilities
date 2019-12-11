e266a743eecd3517775be63a968c157ac758dc30
Shaparev <dshaparev@KLTA.NET>
2019-12-11 18:01:33 +0100

    Refined the script to change file encoding

--- windows/change-encoding.ps1
+++ windows/change-encoding.ps1
@@ -2,12 +2,25 @@ Write-Host "Hi there!"
 $SourceDirectory = "C:\temp"
 $FileFilter = "*.txt"
 $SourceEncodingName = "windows-1251"
-$DestinationEncodingName = "utf-8"
-$DestinationEncodingCode = "UTF8"
+$TargetEncodingName = "utf-8"
+$TargetEncodingCode = "UTF8"
+# Read-Host function wrapper to support default value
+Function Read-Host-Default([String]$Prompt, [String]$DefaultValue) {
+	$ReadResult = $DefaultValue
+    $Value = Read-Host "$Prompt or [Enter] to accept the default ($DefaultValue)"
+    if (!$Value -eq "") {$ReadResult = $Value}
+	return $ReadResult
+}
+# Get input values
+$SourceDirectory = Read-Host-Default "Please enter the source directory" $SourceDirectory
+$FileFilter = Read-Host-Default "Press enter the file filter" $FileFilter
+$SourceEncodingName = Read-Host-Default "Press enter source encoding name" $SourceEncodingName
+$TargetEncodingName = Read-Host-Default "Press enter target encoding name" $TargetEncodingName
+$TargetEncodingCode = Read-Host-Default "Press enter target encoding code" $TargetEncodingCode
 try {
 	# Get encodings
 	$SourceEncoding = [System.Text.Encoding]::GetEncoding($SourceEncodingName)
-	$DestinationEncoding = [System.Text.Encoding]::GetEncoding($DestinatioEncodingName)
+	$TargetEncoding = [System.Text.Encoding]::GetEncoding($DestinatioEncodingName)
 	# Get a list of the source files
 	$FileCount = Get-ChildItem -Path $SourceDirectory -Recurse -File -Filter $FileFilter | Measure-Object | %{$_.Count}
 	$FileList = Get-ChildItem -Path $SourceDirectory -Recurse -File -Filter $FileFilter
@@ -19,16 +32,16 @@ try {
 		Write-Progress -Activity "Changing of files encoding in progress" -Status "$PercentComplete% Complete:" -PercentComplete $PercentComplete
 		try {
 			$Bytes = Get-Content -Path $File.FullName -Encoding Byte -ReadCount 0
-			$Content = [System.Text.Encoding]::Convert($SourceEncoding, $DestinationEncoding, $Bytes)
-			Set-Content -Path $File.FullName $DestinationEncoding.getString($Content) -Encoding $DestinationEncodingCode
+			$Content = [System.Text.Encoding]::Convert($SourceEncoding, $TargetEncoding, $Bytes)
+			Set-Content -Path $File.FullName $TargetEncoding.getString($Content) -Encoding $TargetEncodingCode
 		} catch {
 			Write-Error -Message ("Error occured during processing of " + $File.FullName + " file, Error: " + $_.Exception.Message) 
-				-Category ParserError -ErrorId "E-003"
+				-Category ParserError -ErrorId "E-001"
 		}
 	}
 } catch [System.IO.IOException] {
-	Write-Error -Message ("Directory " + $SrcDir + " does not exist") -Category InvalidArgument -ErrorId "E-002"
+	Write-Error -Message ("Directory " + $SrcDir + " does not exist") -Category InvalidArgument -ErrorId "F-002"
 } catch {
-	Write-Error -Message $_.Exception.Message -Category InvalidArgument -ErrorId "E-001"
+	Write-Error -Message $_.Exception.Message -Category InvalidArgument -ErrorId "F-001"
 }
-Read-Host "Press [Enter] to continue"
+Read-Host "Done. Press [Enter] to continue"
